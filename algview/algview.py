7cf4700f82be4f663f5a5bdbb1877a6c2373adc7
Christoph Jabs <christoph.jabs@helsinki.fi>
2022-04-11 16:11:18 +0300

    first plots

--- algview/algview.py
+++ algview/algview.py
@@ -2,6 +2,8 @@
 
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output, State
+import plotly.graph_objects as go
+import plotly.express as px
 import dash_bootstrap_components as dbc
 
 # === Load Data ===
@@ -10,6 +12,11 @@ import dash_bootstrap_components as dbc
 mlic_data = pd.read_pickle('data/mlic_data.pkl.gz')
 scep_data = pd.read_pickle('data/scep_data.pkl.gz')
 scsc_data = pd.read_pickle('data/scsc_data.pkl.gz')
+
+# Cactus Data
+cactus_include = ['time', 'instance', 'alg', 'timeout', 'memout']
+time_data = pd.concat([mlic_data[cactus_include],
+                       scep_data[cactus_include], scsc_data[cactus_include]])
 
 # Instance data
 mlic_inst_data = pd.read_pickle('data/mlic_instances.pkl.gz')
@@ -25,17 +32,73 @@ scsc_insts = scsc_data['instance'].unique()
 insts = pd.concat(
     [mlic_data['instance'], scep_data['instance'], scsc_data['instance']]).unique()
 
-# === State ===
+# === Colour Scale ===
 
-init_filter = {
-    'algs': algs,
-    'insts': insts,
-    'single_inst': None,
-}
+colour_scale = [
+    # Tsitsul normal 12 colours colour scale
+    # http://tsitsul.in/blog/coloropt/
+    '#ebac23',
+    '#b80058',
+    '#008cf9',
+    '#006e00',
+    '#00bbad',
+    '#d163e6',
+    '#b24502',
+    '#ff9287',
+    '#5954d6',
+    '#00c6f8',
+    '#878500',
+    '#00a75c',
+    '#bdbdbd',
+]
+dash_scale = ['solid', 'dash', 'dot', 'dashdot', 'longdash', 'longdashdot']
+
+
+def alg_colour_dash_scale(alg):
+    toks = alg.split('-')
+    if toks[0] == 'bioptsat':
+        if toks[1] == 'msh':
+            colour = colour_scale[0]
+        elif toks[1] == 'su':
+            colour = colour_scale[1]
+        elif toks[1] == 'msu3':
+            colour = colour_scale[2]
+        elif toks[1] == 'us':
+            colour = colour_scale[3]
+        elif toks[1] == 'oll':
+            colour = colour_scale[4]
+        else:
+            colour = colour_scale[hash(toks[1]) % len(colour_scale)]
+
+        if len(toks) <= 2:
+            dash = dash_scale[0]
+        else:
+            dash = dash_scale[(hash('-'.join(toks[2:])) %
+                               (len(dash_scale) - 1)) + 1]
+    elif toks[0] == 'pminimal':
+        colour = colour_scale[5]
+        dash = dash_scale[0]
+    elif toks[0] == 'seesaw':
+        colour = colour_scale[6]
+        if len(toks) <= 1:
+            dash = dash_scale[0]
+        else:
+            dash = dash_scale[(hash('-'.join(toks[1:])) %
+                               (len(dash_scale) - 1)) + 1]
+    elif toks[0] == 'paretomcs':
+        colour = colour_scale[7]
+        if len(toks) <= 1:
+            dash = dash_scale[0]
+        else:
+            dash = dash_scale[(hash('-'.join(toks[1:])) %
+                               (len(dash_scale) - 1)) + 1]
+
+    return colour, dash
 
 # === Application Layout ===
 
 # --- Reusable Styles ---
+
 
 box_style = {'padding': '10px', 'border': '2px solid black'}
 
@@ -43,27 +106,32 @@ box_style = {'padding': '10px', 'border': '2px solid black'}
 
 alg_selector = html.Div([
     html.H5('Algorithms'),
-    html.Div(id='div-alg-sel', children=[dcc.Checklist(options=algs, value=init_filter['algs'],
-             id='checklist-alg')], style={'maxHeight': '400px', 'overflow': 'scroll'}),
+    html.Div(id='div-alg-sel', children=[dcc.Checklist(options=algs, value=algs,
+             id='checklist-alg')], style={'height': '400px', 'maxHeight': '400px', 'overflow': 'scroll'}),
 ], style=box_style)
 
 inst_selector_check = dcc.Checklist(
-    options=insts, value=init_filter['insts'], id='checklist-inst')
+    options=insts, value=insts, id='checklist-inst')
 inst_selector_radio = dcc.RadioItems(
-    options=insts, value=init_filter['single_inst'], id='radio-inst')
+    options=insts, value=None, id='radio-inst')
 
 inst_selector = html.Div([
     html.H5('Instances'),
     html.Div(id='div-inst-sel', children=[inst_selector_check],
-             style={'maxHeight': '400px', 'overflow': 'scroll'}),
+             style={'height': '400px', 'maxHeight': '400px', 'overflow': 'scroll'}),
 ], style=box_style)
 
 alg_right = [
     dcc.Tabs(id='tabs-view', value='cactus-view', children=[
-        dcc.Tab(label='Overview', value='cactus-view'),
+        dcc.Tab(label='Overview - Cactus and Histogram', value='cactus-view'),
         dcc.Tab(label='Pairwise Comparison', value='scatter-view'),
     ]),
     html.Div(id='div-view')
+]
+
+cactus_graphs = [
+    dcc.Graph(id='cactus-plot'),
+    dcc.Graph(id='hist-plot'),
 ]
 
 # --- Main Layout ---
@@ -92,7 +160,6 @@ app.layout = html.Div(
             ]), width=4),
             dbc.Col(html.Div(id='div-right', style=box_style))
         ])]),
-        dcc.Store(id='current-filter', data=init_filter),
     ], style={'margin': '20px'}
 )
 
@@ -115,26 +182,67 @@ def render_page(page):
 
 @ app.callback(
     Output('div-view', 'children'),
-    Input('tabs-views', 'value'),
+    Input('tabs-view', 'value'),
 )
 def render_view(view):
     if view == 'cactus-view':
-        return None
+        return cactus_graphs
     elif view == 'scatter-view':
         return None
 
 
-@ app.callback(
-    Output('current-filter', 'data'),
+@app.callback(
+    Output('cactus-plot', 'figure'),
     Input('checklist-alg', 'value'),
     Input('checklist-inst', 'value'),
-    Input('radio-inst', 'value'),
-    State('current-state', 'data'),
 )
-def update_state(algs, insts, inst, state):
-    state['algs'] = algs
-    state['insts'] = insts
-    state['single_inst'] = inst
+def update_cactus(algs, insts):
+    data = time_data[time_data['alg'].isin(algs) & time_data['instance'].isin(insts)
+                     & ~time_data['timeout'] & ~time_data['memout']].sort_values('time')
+
+    for a in algs:
+        data.loc[data['alg'] == a, 'rank'] = range(
+            1, (data['alg'] == a).sum()+1)
+
+    fig = go.Figure()
+    for i, a in enumerate(data['alg'].unique()):
+        colour, dash = alg_colour_dash_scale(a)
+        fig.add_trace(go.Scatter(x=data.loc[data['alg'] == a, 'rank'], y=data.loc[data['alg'] == a, 'time'],
+                                 hovertemplate='<b>algorithm: ' + a +
+                                 '</b><br><i># instances: %{x}<br>cpu time: %{y}s</i><br>instance: %{text}',
+                                 text=data.loc[data['alg'] == a,
+                                               'instance'], name=a, mode='lines+markers',
+                                 line=dict(color=colour, dash=dash), marker=dict(size=5)))
+
+    fig.update_xaxes(title_text='# instances')
+    fig.update_yaxes(title_text='cpu time')
+
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0))
+
+    return fig
+
+
+@app.callback(
+    Output('hist-plot', 'figure'),
+    Input('checklist-alg', 'value'),
+    Input('checklist-inst', 'value'),
+)
+def update_hist(algs, insts):
+    data = time_data[time_data['alg'].isin(
+        algs) & time_data['instance'].isin(insts)]
+
+    fig = go.Figure()
+    for i, a in enumerate(data['alg'].unique()):
+        colour, dash = alg_colour_dash_scale(a)
+        fig.add_trace(go.Histogram(
+            x=data.loc[data['alg'] == a, 'time'], name=a, marker=dict(color=colour)))
+
+    fig.update_xaxes(title_text='cpu time')
+    fig.update_yaxes(title_text='# runs')
+
+    fig.update_layout(margin=dict(l=0, r=0, b=0, t=0), barmode='stack')
+
+    return fig
 
 
 # === Main ===
